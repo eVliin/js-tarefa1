0707c520357ed33aac9a9ebd7d26b8a5ee695daa
eVliin <evelynma.b7@gmail.com>
2022-06-10 19:15:15 -0300

    v1

---
+++ script.js
@@ -0,0 +1,11 @@
+let nome = "jotaro";
+let idade = 17;
+let nascimento = idade - 2022;
+
+console.log(nascimento);
+console.log("Estou mexendo no console.log");
+
+let quartaFeira = false;
+
+console.log(quartaFeira);
+console.log(typeof quartaFeira);
